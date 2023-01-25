614839a9d35caaf9ac9352659324e9d1fb9bdc8c
清 <claude-zq@qq.com>
2023-01-25 13:22:15 +0800

    feat: add jwt

--- server/shared/consts/consts.go
+++ server/shared/consts/consts.go
@@ -1,11 +1,17 @@
 package consts
 
+import "time"
+
 const (
 	MySqlDSN       = "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
 	UserMigrateDSN = "root:123456@tcp(localhost:3306)/TikGok?charset=utf8mb4&parseTime=True&loc=Local"
 
-	JWTIssuer  = "TikGok"
-	ThirtyDays = 60 * 60 * 24 * 30
+	JWTIssuer        = "TikGok"
+	ThirtyDays       = 60 * 60 * 24 * 30
+	AuthorizationKey = "authorization"
+	TokenRefreshTime = time.Hour
+	Claims           = "claims"
+	AccountID        = "accountID"
 
 	ApiConfigPath         = "./server/cmd/api/config.yaml"
 	UserConfigPath        = "./server/cmd/user/config.yaml"
---
+++ server/shared/middleware/jwt.go
@@ -0,0 +1,110 @@
+package middleware
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/CyanAsterisk/TikGok/server/cmd/api/biz/errno"
+	"github.com/CyanAsterisk/TikGok/server/cmd/api/global"
+	"github.com/CyanAsterisk/TikGok/server/cmd/api/model"
+	"github.com/CyanAsterisk/TikGok/server/shared/consts"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/golang-jwt/jwt"
+)
+
+var (
+	TokenExpired     = errors.New("token is expired")
+	TokenNotValidYet = errors.New("token not active yet")
+	TokenMalformed   = errors.New("that's not even a token")
+	TokenInvalid     = errors.New("couldn't handle this token")
+)
+
+func JWTAuth() app.HandlerFunc {
+	return func(ctx context.Context, c *app.RequestContext) {
+		token := c.Request.Header.Get(consts.AuthorizationKey)
+		if token == "" {
+			errno.SendResponse(c, errno.AuthorizeFail, errors.New("no token"))
+			c.Abort()
+			return
+		}
+		token = strings.Split(token, " ")[1]
+		j := NewJWT()
+		// Parse the information contained in the token
+		claims, err := j.ParseToken(token)
+		if err != nil {
+			errno.SendResponse(c, errno.AuthorizeFail, err)
+			c.Abort()
+			return
+		}
+		c.Set(consts.Claims, claims)
+		c.Set(consts.AccountID, claims.ID)
+		c.Next(ctx)
+	}
+}
+
+type JWT struct {
+	SigningKey []byte
+}
+
+func NewJWT() *JWT {
+	return &JWT{
+		SigningKey: []byte(global.ServerConfig.JWTInfo.SigningKey),
+	}
+}
+
+// CreateToken to create a token
+func (j *JWT) CreateToken(claims models.CustomClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.SigningKey)
+}
+
+// ParseToken to parse a token
+func (j *JWT) ParseToken(tokenString string) (*models.CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &models.CustomClaims{}, func(token *jwt.Token) (i interface{}, e error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, TokenMalformed
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				// Token is expired
+				return nil, TokenExpired
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, TokenNotValidYet
+			} else {
+				return nil, TokenInvalid
+			}
+		}
+	}
+	if token != nil {
+		if claims, ok := token.Claims.(*models.CustomClaims); ok && token.Valid {
+			return claims, nil
+		}
+		return nil, TokenInvalid
+
+	} else {
+		return nil, TokenInvalid
+	}
+}
+
+// RefreshToken to refresh a token
+func (j *JWT) RefreshToken(tokenString string) (string, error) {
+	jwt.TimeFunc = func() time.Time {
+		return time.Unix(0, 0)
+	}
+	token, err := jwt.ParseWithClaims(tokenString, &models.CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if claims, ok := token.Claims.(*models.CustomClaims); ok && token.Valid {
+		jwt.TimeFunc = time.Now
+		claims.StandardClaims.ExpiresAt = time.Now().Add(consts.TokenRefreshTime).Unix()
+		return j.CreateToken(*claims)
+	}
+	return "", TokenInvalid
+}
