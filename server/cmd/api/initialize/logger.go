2d25c72d5d059986ec253b62f39c3b2dda8b5b46
L2ncE <llance_24@foxmail.com>
2023-01-25 20:19:53 +0800

    optimize: improve init logger

--- server/cmd/api/initialize/logger.go
+++ server/cmd/api/initialize/logger.go
@@ -1,14 +1,47 @@
 package initialize
 
 import (
+	"os"
+	"path"
+	"runtime"
+	"time"
+
+	"github.com/CyanAsterisk/TikGok/server/shared/consts"
 	"github.com/cloudwego/hertz/pkg/common/hlog"
 	hertzlogrus "github.com/hertz-contrib/obs-opentelemetry/logging/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 // InitLogger to init logrus
 func InitLogger() {
-	logger := hertzlogrus.NewLogger()
+	// Customizable output directory.
+	logFilePath := consts.HlogFilePath
+	if err := os.MkdirAll(logFilePath, 0o777); err != nil {
+		panic(err)
+	}
 
+	// Set filename to date
+	logFileName := time.Now().Format("2006-01-02") + ".log"
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			panic(err)
+		}
+	}
+
+	logger := hertzlogrus.NewLogger()
+	// Provides compression and deletion
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   fileName,
+		MaxSize:    20,   // A file can be up to 20M.
+		MaxBackups: 5,    // Save up to 5 files at the same time.
+		MaxAge:     10,   // A file can exist for a maximum of 10 days.
+		Compress:   true, // Compress with gzip.
+	}
+
+	if runtime.GOOS == "linux" {
+		logger.SetOutput(lumberjackLogger)
+	}
 	logger.SetLevel(hlog.LevelDebug)
 
 	hlog.SetLogger(logger)
--- server/cmd/user/initialize/logger.go
+++ server/cmd/user/initialize/logger.go
@@ -1,14 +1,47 @@
 package initialize
 
 import (
+	"os"
+	"path"
+	"runtime"
+	"time"
+
+	"github.com/CyanAsterisk/TikGok/server/shared/consts"
 	"github.com/cloudwego/kitex/pkg/klog"
 	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 // InitLogger to init logrus
 func InitLogger() {
-	logger := kitexlogrus.NewLogger()
+	// Customizable output directory.
+	logFilePath := consts.KlogFilePath
+	if err := os.MkdirAll(logFilePath, 0o777); err != nil {
+		panic(err)
+	}
 
+	// Set filename to date
+	logFileName := time.Now().Format("2006-01-02") + ".log"
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			panic(err)
+		}
+	}
+
+	logger := kitexlogrus.NewLogger()
+	// Provides compression and deletion
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   fileName,
+		MaxSize:    20,   // A file can be up to 20M.
+		MaxBackups: 5,    // Save up to 5 files at the same time.
+		MaxAge:     10,   // A file can exist for a maximum of 10 days.
+		Compress:   true, // Compress with gzip.
+	}
+
+	if runtime.GOOS == "linux" {
+		logger.SetOutput(lumberjackLogger)
+	}
 	logger.SetLevel(klog.LevelDebug)
 
 	klog.SetLogger(logger)
