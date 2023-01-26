e88d78007a286a1d5eab41b555c987be72d7eae1
清 <claude-zq@qq.com>
2023-01-26 17:53:51 +0800

    fix: unique username

--- server/cmd/user/model/user.go
+++ server/cmd/user/model/user.go
@@ -9,7 +9,7 @@ import (
 
 type User struct {
 	ID       int64  `gorm:"primarykey"`
-	Username string `gorm:"type:varchar(33);not null"`
+	Username string `gorm:"type:varchar(33);unique;u;not null"`
 	Password string `gorm:"type:varchar(33);not null"`
 }
 
