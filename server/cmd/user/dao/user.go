24407491cb979c56d6f1f0c5c524d0615ff835b3
清 <claude-zq@qq.com>
2023-01-26 17:54:06 +0800

    feat: add user crud

--- server/cmd/user/dao/user.go
+++ server/cmd/user/dao/user.go
@@ -1,1 +1,27 @@
 package dao
+
+import (
+	"github.com/CyanAsterisk/TikGok/server/cmd/user/global"
+	"github.com/CyanAsterisk/TikGok/server/cmd/user/model"
+)
+
+// GetUserByUsername get user by username
+func GetUserByUsername(username string) (*model.User, error) {
+	var user model.User
+	err := global.DB.Model(&model.User{}).
+		Where(&model.User{Username: username}).First(&user).Error
+	return &user, err
+}
+
+// GetUserById get user by username
+func GetUserById(uid int64) (*model.User, error) {
+	var user model.User
+	err := global.DB.Model(&model.User{}).
+		Where(&model.User{ID: uid}).First(&user).Error
+	return &user, err
+}
+
+// CreateUser creates a user.
+func CreateUser(user *model.User) error {
+	return global.DB.Model(&model.User{}).Create(user).Error
+}
