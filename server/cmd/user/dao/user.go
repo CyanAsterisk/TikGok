3405ddfa92a7c6082cc2b0c27843dc16817766da
清 <claude-zq@qq.com>
2023-01-31 14:56:51 +0800

    fix: fix user dao

--- server/cmd/user/dao/user.go
+++ server/cmd/user/dao/user.go
@@ -1,8 +1,16 @@
 package dao
 
 import (
+	"errors"
+
 	"github.com/CyanAsterisk/TikGok/server/cmd/user/global"
 	"github.com/CyanAsterisk/TikGok/server/cmd/user/model"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrNoSuchUser = errors.New("no such user")
+	ErrUserExist  = errors.New("user already exist")
 )
 
 // GetUserByUsername get user by username
@@ -10,6 +18,9 @@ func GetUserByUsername(username string) (*model.User, error) {
 	var user model.User
 	err := global.DB.Model(&model.User{}).
 		Where(&model.User{Username: username}).First(&user).Error
+	if err == gorm.ErrRecordNotFound {
+		return nil, ErrNoSuchUser
+	}
 	return &user, err
 }
 
@@ -18,10 +29,20 @@ func GetUserById(uid int64) (*model.User, error) {
 	var user model.User
 	err := global.DB.Model(&model.User{}).
 		Where(&model.User{ID: uid}).First(&user).Error
+	if err == gorm.ErrRecordNotFound {
+		return nil, ErrNoSuchUser
+	}
 	return &user, err
 }
 
 // CreateUser creates a user.
 func CreateUser(user *model.User) error {
+	err := global.DB.Model(&model.User{}).
+		Where(&model.User{Username: user.Username}).First(&user).Error
+	if err == nil {
+		return ErrUserExist
+	} else if err != gorm.ErrRecordNotFound {
+		return err
+	}
 	return global.DB.Model(&model.User{}).Create(user).Error
 }
