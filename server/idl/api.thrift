27e1e8a333fc9df4ba4acdf066c3dd5ed1d0beb8
L2ncE <llance_24@foxmail.com>
2023-02-01 21:04:10 +0800

    refactor: improve IDL

--- server/idl/api.thrift
+++ server/idl/api.thrift
@@ -157,7 +157,28 @@ struct douyin_relation_friend_list_request {
 struct douyin_relation_friend_list_response {
     1: i32 status_code // Status code, 0-success, other values-failure
     2: string status_msg // Return status description
-    3: list<base.User> user_list // List of user information
+    3: required list<base.FriendUser> user_list,     // List of user information
+}
+
+struct douyin_message_chat_request {
+    1: i64 user_id // User Id
+    2: i64 to_user_id // The other party's user id
+}
+
+struct douyin_message_chat_response {
+    1: base.douyin_base_response base_resp
+    2: list<base.Message> message_list // Message list
+}
+
+struct douyin_message_action_request {
+    1: i64 user_id // User Id
+    2: i64 to_user_id // The other party's user id
+    3: i8 action_type // 1- Send a message
+    4: string content // Message content
+}
+
+struct douyin_message_action_response {
+    1: base.douyin_base_response base_resp
 }
 
 service ApiService {
@@ -178,4 +199,7 @@ service ApiService {
     douyin_relation_follow_list_response FollowingList(1: douyin_relation_follow_list_request req)(api.get="/douyin/relation/follow/list");
     douyin_relation_follower_list_response FollowerList(1: douyin_relation_follower_list_request req)(api.get="/douyin/relation/follower/list");
     douyin_relation_friend_list_response FriendList(1: douyin_relation_friend_list_request req)(api.get="/douyin/relation/friend/list");
+
+    douyin_message_chat_response ChatHistory(1: douyin_message_chat_request req)(api.get="/douyin/message/chat");
+    douyin_message_action_response SentMessage(1: douyin_message_action_request req)(api.post="/douyin/message/chat");
 }
