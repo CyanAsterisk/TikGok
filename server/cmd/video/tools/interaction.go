7d719e86c6ec88f25e5b763d58a8888a3827023c
清 <claude-zq@qq.com>
2023-02-01 22:39:05 +0800

    fix: update interaction manager

--- server/cmd/video/tools/interaction.go
+++ server/cmd/video/tools/interaction.go
@@ -2,10 +2,11 @@ package tools
 
 import (
 	"context"
-	"github.com/CyanAsterisk/TikGok/server/shared/kitex_gen/interaction"
 
 	"github.com/CyanAsterisk/TikGok/server/shared/errno"
+	"github.com/CyanAsterisk/TikGok/server/shared/kitex_gen/interaction"
 	"github.com/CyanAsterisk/TikGok/server/shared/kitex_gen/interaction/interactionserver"
+	"github.com/cloudwego/kitex/pkg/klog"
 )
 
 type InteractionManager struct {
@@ -14,34 +15,34 @@ type InteractionManager struct {
 
 // GetCommentCount  get one video comment.
 func (i *InteractionManager) GetCommentCount(ctx context.Context, videoId int64) (int64, error) {
-	resp, err := i.InteractionService.CommentList(ctx, &interaction.DouyinCommentListRequest{VideoId: videoId})
+	res, err := i.InteractionService.CommentCount(ctx, &interaction.DouyinCommentCountRequest{VideoId: videoId})
 	if err != nil {
+		klog.Errorf("get comment count err", err.Error())
 		return 0, err
 	}
-	if int64(resp.BaseResp.StatusCode) != errno.Success.ErrCode {
-		return 0, errno.InteractionServerErr.WithMessage(resp.BaseResp.StatusMsg)
+	if res.BaseResp.StatusCode != int32(errno.Success.ErrCode) {
+		klog.Errorf("get comment count err", res.BaseResp.StatusMsg)
+		return 0, errno.ServiceErr.WithMessage(res.BaseResp.StatusMsg)
 	}
-	return int64(len(resp.CommentList)), nil
+	return res.Count, nil
 }
 
 // CheckFavorite check one favorite the video or not.
 func (i *InteractionManager) CheckFavorite(ctx context.Context, userId int64, videoId int64) (bool, error) {
-	resp, err := i.InteractionService.FavoriteList(ctx, &interaction.DouyinFavoriteListRequest{OwnerId: userId})
-	if err != nil {
-		return false, err
-	}
-	if int64(resp.BaseResp.StatusCode) != errno.Success.ErrCode {
-		return false, errno.InteractionServerErr.WithMessage(resp.BaseResp.StatusMsg)
-	}
-	for _, v := range resp.VideoList {
-		if v.Id == videoId {
-			return true, nil
-		}
-	}
+	//TODO: add rpc api
 	return false, nil
 }
 
 // GetFavoriteCount get the favorite num of the video.
 func (i *InteractionManager) GetFavoriteCount(ctx context.Context, videoId int64) (int64, error) {
-	return 0, nil
+	res, err := i.InteractionService.FavoriteCount(ctx, &interaction.DouyinFavoriteCountRequest{VideoId: videoId})
+	if err != nil {
+		klog.Errorf("get favorite count err", err.Error())
+		return 0, err
+	}
+	if res.BaseResp.StatusCode != int32(errno.Success.ErrCode) {
+		klog.Errorf("get favorite count err", res.BaseResp.StatusMsg)
+		return 0, errno.ServiceErr.WithMessage(res.BaseResp.StatusMsg)
+	}
+	return res.Count, nil
 }
