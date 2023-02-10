9b1e69c7784e3fd660a16a5cc81782af3fee05f1
清 <claude-zq@qq.com>
2023-02-10 21:32:07 +0800

    fix: fix bug and modify logger

--- server/shared/test/mysql.go
+++ server/shared/test/mysql.go
@@ -3,8 +3,11 @@ package test
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/CyanAsterisk/TikGok/server/shared/consts"
 	"github.com/docker/docker/api/types"
@@ -13,6 +16,7 @@ import (
 	"github.com/docker/go-connections/nat"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 )
 
@@ -21,7 +25,7 @@ var mysqlDSN string
 // RunWithMySQLInDocker runs the tests with
 // a MySQL instance in a docker container.
 func RunWithMySQLInDocker(m *testing.M) int {
-	c, err := client.NewClientWithOpts()
+	c, err := client.NewClientWithOpts(client.WithVersion("1.41"))
 	if err != nil {
 		panic(err)
 	}
@@ -33,6 +37,7 @@ func RunWithMySQLInDocker(m *testing.M) int {
 		ExposedPorts: nat.PortSet{
 			consts.MySQLContainerPort: {},
 		},
+		Env: []string{"MYSQL_DATABASE=" + consts.TikGok, "MYSQL_ROOT_PASSWORD=" + consts.DockerTestMySQLPwd},
 	}, &container.HostConfig{
 		PortBindings: nat.PortMap{
 			consts.MySQLContainerPort: []nat.PortBinding{
@@ -67,16 +72,27 @@ func RunWithMySQLInDocker(m *testing.M) int {
 	}
 	hostPort := inspRes.NetworkSettings.Ports[consts.MySQLContainerPort][0]
 	port, _ := strconv.Atoi(hostPort.HostPort)
-	mysqlDSN = fmt.Sprintf(consts.MySqlDSN, consts.MySQLAdmin, "", hostPort.HostIP, port, consts.TikGok)
+	mysqlDSN = fmt.Sprintf(consts.MySqlDSN, consts.MySQLAdmin, consts.DockerTestMySQLPwd, hostPort.HostIP, port, consts.TikGok)
 
 	return m.Run()
 }
 
-func NewTestDB() *gorm.DB {
+func NewTestMysqlDB() *gorm.DB {
+
+	time.Sleep(time.Second * 15) // Wait the container start. TODO: need to optimize
 	db, err := gorm.Open(mysql.Open(mysqlDSN), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true,
 		},
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logger.Warn,
+				IgnoreRecordNotFoundError: true,
+				Colorful:                  false,
+			},
+		),
 	})
 	if err != nil {
 		panic(err)
